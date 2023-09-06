bf7e9397f07190f0621a4927903821a273041925
Viviane Martini <martini.viviane@gmail.com>
2023-09-06 17:10:14 -0300

    response.json

--- src/server.js
+++ src/server.js
@@ -23,7 +23,7 @@ app.use(express.json())
 app.post('/users', (request, response) => {
     const { name, email, password } = request.body
 
-    response.send(`Usuário: ${name}, email: ${email}, senha: ${password}`)
+    response.json({ name, email, password })
 })
 
 
