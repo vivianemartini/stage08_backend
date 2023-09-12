3cb45c5ca21f6ce7fe3764268cc8355afb54aede
Viviane Martini <martini.viviane@gmail.com>
2023-09-12 18:54:23 -0300

    esl lint

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -1,6 +1,6 @@
 //a classe permite criar varias funções e acessar elas
 class UsersController {
- /*     Um controller pode ter no máximo 5 métodos:
+  /*     Um controller pode ter no máximo 5 métodos:
 
     * método index - GET para listar vários registros
     * método show - GET para exibir um registro especifico
@@ -9,12 +9,11 @@ class UsersController {
     * método delete - DELETE para remover um registro
  */
 
-    create(request, response) {
-        const { name, email, password } = request.body
+  create(request, response) {
+    const { name, email, password } = request.body
 
-        response.json({ name, email, password })
-    }
+    response.json({ name, email, password })
+  }
 }
-
 
 module.exports = UsersController
