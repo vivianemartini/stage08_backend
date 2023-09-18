73f288a81192a58dfa29eec22496a1c68c44e069
Viviane Martini <martini.viviane@gmail.com>
2023-09-18 11:15:17 -0300

    insert user into data base

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -24,6 +24,8 @@ class UsersController {
       throw new AppError('Este e-mail já está em uso')
     }
 
+    await database.run('INSERT INTO users (name, email, password) VALUES (?, ?, ?)', [name, email, password])
+
     return response.status(201).json()
   }
 }
